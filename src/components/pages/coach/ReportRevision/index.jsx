f019d16743859635b5596fa6f218a34748253c18
christianaichner <contact@aichner-christian.com>
2019-08-07 14:04:08 +0200

    Add report revision content
    Static data for design testing has been added

--- src/components/pages/coach/ReportRevision/index.jsx
+++ src/components/pages/coach/ReportRevision/index.jsx
@@ -2,13 +2,125 @@
 // Contains all the functionality necessary to define React components
 import React from 'react';
 
-// This returns null because to this point nothing should be displayed
+//> MDB
+// "Material Design for Bootstrap" is a great UI design framework
+import {
+  MDBContainer,
+  MDBCollapse,
+  MDBCard,
+  MDBCardBody,
+  MDBCollapseHeader,
+  MDBProgress,
+  MDBRow,
+  MDBCol,
+  MDBInput,
+} from 'mdbreact';
+
 class ReportRevision extends React.Component{
-    render(){
-        return(
-            null
-        )
+    state={
+        collapse1: true,
+        collapse2: true,
+        collapse3: true,
     }
+
+    toggleCollapse = collapseID => () =>
+    this.setState({
+        [collapseID]: !(this.state[collapseID])
+    });
+
+render() {
+    console.log(this.state);
+  return (
+    <MDBContainer>
+      <MDBContainer className="mt-5">
+        <h4>Review individual beauty report</h4>
+        <MDBProgress material value={33} height="20px">
+            33%
+        </MDBProgress>
+        <MDBCard className="mt-3">
+          <MDBCollapseHeader>
+            <MDBRow className="justify-content-center">
+                <MDBCol md="6" className="align-self-center">
+                    Intro
+                </MDBCol>
+                <MDBCol md="6" className="text-right">
+                    <MDBInput label="Überprüft" filled type="checkbox" id="checkbox1" onClick={this.toggleCollapse("collapse1")} />
+                </MDBCol>
+            </MDBRow>
+          </MDBCollapseHeader>
+          <MDBCollapse id="collapse1" isOpen={this.state.collapse1}>
+            <MDBCardBody>
+              Pariatur cliche reprehenderit, enim eiusmod high life accusamus
+              terry richardson ad squid. 3 wolf moon officia aute, non
+              cupidatat skateboard dolor brunch. Food truck quinoa nesciunt
+              laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a
+              bird on it squid single-origin coffee nulla assumenda shoreditch
+              et. Nihil anim keffiyeh helvetica, craft beer labore wes
+              anderson cred nesciunt sapiente ea proident. Ad vegan excepteur
+              butcher vice lomo. Leggings occaecat craft beer farm-to-table,
+              raw denim aesthetic synth nesciunt you probably haven&apos;t
+              heard of them accusamus labore sustainable VHS.
+            </MDBCardBody>
+          </MDBCollapse>
+        </MDBCard>
+
+        <MDBCard>
+          <MDBCollapseHeader>
+            <MDBRow className="justify-content-center">
+                <MDBCol md="6" className="align-self-center">
+                    Hautzustand
+                </MDBCol>
+                <MDBCol md="6" className="text-right">
+                    <MDBInput label="Überprüft" filled type="checkbox" id="checkbox2" onClick={this.toggleCollapse("collapse2")} />
+                </MDBCol>
+            </MDBRow>
+          </MDBCollapseHeader>
+          <MDBCollapse id="collapse2" isOpen={this.state.collapse2}>
+            <MDBCardBody>
+              Anim pariatur cliche reprehenderit, enim eiusmod high life
+              accusamus terry richardson ad squid. 3 wolf moon officia aute,
+              non cupidatat skateboard dolor brunch. Food truck quinoa
+              nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua
+              put a bird on it squid single-origin coffee nulla assumenda
+              shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
+              wes anderson cred nesciunt sapiente ea proident. Ad vegan
+              excepteur butcher vice lomo. Leggings occaecat craft beer
+              farm-to-table, raw denim aesthetic synth nesciunt you probably
+              haven&apos;t heard of them accusamus labore sustainable VHS.
+            </MDBCardBody>
+          </MDBCollapse>
+        </MDBCard>
+
+        <MDBCard>
+           <MDBCollapseHeader>
+            <MDBRow className="justify-content-center">
+                <MDBCol md="6" className="align-self-center">
+                    Intro
+                </MDBCol>
+                <MDBCol md="6" className="text-right">
+                    <MDBInput label="Überprüft" filled type="checkbox" id="checkbox3" onClick={this.toggleCollapse("collapse3")} />
+                </MDBCol>
+            </MDBRow>
+          </MDBCollapseHeader>
+          <MDBCollapse id="collapse3" isOpen={this.state.collapse3}>
+            <MDBCardBody>
+              Anim pariatur cliche reprehenderit, enim eiusmod high life
+              accusamus terry richardson ad squid. 3 wolf moon officia aute,
+              non cupidatat skateboard dolor brunch. Food truck quinoa
+              nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua
+              put a bird on it squid single-origin coffee nulla assumenda
+              shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
+              wes anderson cred nesciunt sapiente ea proident. Ad vegan
+              excepteur butcher vice lomo. Leggings occaecat craft beer
+              farm-to-table, raw denim aesthetic synth nesciunt you probably
+              haven&apos;t heard of them accusamus labore sustainable VHS.
+            </MDBCardBody>
+          </MDBCollapse>
+        </MDBCard>
+      </MDBContainer>
+    </MDBContainer>
+    );
+  }
 }
 
 export default ReportRevision;
