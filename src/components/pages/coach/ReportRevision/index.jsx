cc0bb09f276008b0ad7953bef77039bb01af5043
Christian Aichner <contact@aichner-christian.com>
2019-08-10 18:26:56 +0200

    Improve dummy data handling
    The dummy data has been improved. First, the
    values were nested objects in an object. Now
    they are objects in an array. This makes all
    things way cleaner and more organized. Also
    it improves performance, because the JS ES5
    OBJECT does not need to be called every time.

--- src/components/pages/coach/ReportRevision/index.jsx
+++ src/components/pages/coach/ReportRevision/index.jsx
@@ -75,23 +75,32 @@ const GET_SECTIONS_DATA = gql`
 const report = {
     customer: "Erika Mustermann",
     coach: "Monika Mustermann",
-    sections: {
-        intro: "ABER…! In diesem Thema müssen wir etwas ausholen, da es essentiell für deine Haut ist:<br/>Es gibt oft Verwirrung darüber, ob neben Produkten wie einer Feuchtigkeitscreme oder Grundierung mit Lichtschutzfaktor zusätzlich ein Sonnenschutzmittel verwendet werden sollte. Die Wahrheit ist, dass die meisten von uns nicht genug von unserem Kosmetikprodukt verwenden, um den gleichen Lichtschutzfaktor zu erzielen wie ein Sonnenschutzmittel allein. Im Frühjahr und Sommer lohnt es sich, ein separates Sonnenschutzmittel zu verwenden.<br/>Es ist jedoch nicht so, dass ein einheitliches Modell für alle gilt. Es muss bei der Auswahl eines Sonnenschutzes vielmehr auch ein gesunder Menschenverstand eingesetzt werden. Wenn du beispielsweise im Winter regelmäßig Peeling-Produkte wie Alpha-Hydroxy-Säuren (AHAs)  und Beta-Hydroxysäuren (BHAs)  verwendest, ist ein separater Sonnenschutz besser geeignet. Wenn du an Outdoor-Aktivitäten teilnimmst oder beruflich länger als zwanzig Minuten im Freien bist, solltest du einen täglichen Sonnenschutz verwenden. Gleiches gilt, wenn du dich in einem sonnigen Klima oder in der Nähe des Äquators befindest: Das Tragen eines regelmäßigen täglichen ",
-        hautzustand: "die Verwendung eines LSF 30 keinen doppelt so hohen Schutz bietet, wie die Verwendung von LSF 15? Ebenso ist ein Faktor 20 nicht doppelt so gut wie ein Faktor 10. Ein LSF 15 blockiert etwa 93% der UVB Strahlung. Ein LSF 30 blockiert etwa 97% und LSF 50 blockiert etwa 98% der UVB Strahlung. Du siehst also, dass zwischen einem LSF von 30 und 50 tatsächlich kaum Unterschiede bestehen. Es gibt keinen Sonnenschutz, der 100% igen Schutz vor der Sonne bietet.",
-        ending: "Das sagen unsere Experten<br/>Das natürliche Altern der Haut steht in direkter Verbindung mit der Sauerstoffversorgung. Schon ab dem 20. Lebensjahr beginnen die Kapillaren dem Gewebe immer weniger Sauerstoff zuzuführen, wofür eine altersbedingte Verengung der Gefäße verantwortlich ist (der Aufenthalt in geschlossenen Räumen kann diese Tendenz verstärken). Durch die verminderte Sauerstoffzufuhr werden nicht genügend Nährstoffe zu den Zellen transportiert, so dass diese allmählich „verhungern“. Wer sich regelmäßig an der frischen Luft aufhält, pumpt jede Menge Sauerstoff in seine Lungen: 95 Prozent über die Atmung und fünf Prozent über die Haut. In den Zellen bewirkt der Sauerstoff einen Teilungsimpuls, der die Haut jünger und frischer aussehen lässt.",
-    }
+    sections: [
+        {
+            title: "Intro",
+            text: "ABER…! In diesem Thema müssen wir etwas ausholen, da es essentiell für deine Haut ist:<br/>Es gibt oft Verwirrung darüber, ob neben Produkten wie einer Feuchtigkeitscreme oder Grundierung mit Lichtschutzfaktor zusätzlich ein Sonnenschutzmittel verwendet werden sollte. Die Wahrheit ist, dass die meisten von uns nicht genug von unserem Kosmetikprodukt verwenden, um den gleichen Lichtschutzfaktor zu erzielen wie ein Sonnenschutzmittel allein. Im Frühjahr und Sommer lohnt es sich, ein separates Sonnenschutzmittel zu verwenden.<br/>Es ist jedoch nicht so, dass ein einheitliches Modell für alle gilt. Es muss bei der Auswahl eines Sonnenschutzes vielmehr auch ein gesunder Menschenverstand eingesetzt werden. Wenn du beispielsweise im Winter regelmäßig Peeling-Produkte wie Alpha-Hydroxy-Säuren (AHAs)  und Beta-Hydroxysäuren (BHAs)  verwendest, ist ein separater Sonnenschutz besser geeignet. Wenn du an Outdoor-Aktivitäten teilnimmst oder beruflich länger als zwanzig Minuten im Freien bist, solltest du einen täglichen Sonnenschutz verwenden. Gleiches gilt, wenn du dich in einem sonnigen Klima oder in der Nähe des Äquators befindest: Das Tragen eines regelmäßigen täglichen "
+        },
+        {
+            title: "Hautzustand",
+            text: "die Verwendung eines LSF 30 keinen doppelt so hohen Schutz bietet, wie die Verwendung von LSF 15? Ebenso ist ein Faktor 20 nicht doppelt so gut wie ein Faktor 10. Ein LSF 15 blockiert etwa 93% der UVB Strahlung. Ein LSF 30 blockiert etwa 97% und LSF 50 blockiert etwa 98% der UVB Strahlung. Du siehst also, dass zwischen einem LSF von 30 und 50 tatsächlich kaum Unterschiede bestehen. Es gibt keinen Sonnenschutz, der 100% igen Schutz vor der Sonne bietet."
+        },
+        {
+            title: "Ending",
+            text: "Das sagen unsere Experten<br/>Das natürliche Altern der Haut steht in direkter Verbindung mit der Sauerstoffversorgung. Schon ab dem 20. Lebensjahr beginnen die Kapillaren dem Gewebe immer weniger Sauerstoff zuzuführen, wofür eine altersbedingte Verengung der Gefäße verantwortlich ist (der Aufenthalt in geschlossenen Räumen kann diese Tendenz verstärken). Durch die verminderte Sauerstoffzufuhr werden nicht genügend Nährstoffe zu den Zellen transportiert, so dass diese allmählich „verhungern“. Wer sich regelmäßig an der frischen Luft aufhält, pumpt jede Menge Sauerstoff in seine Lungen: 95 Prozent über die Atmung und fünf Prozent über die Haut. In den Zellen bewirkt der Sauerstoff einen Teilungsimpuls, der die Haut jünger und frischer aussehen lässt."
+        }
+    ]
 }
 
 class ReportRevision extends React.Component{
 
     componentWillMount = () => {
-        Object.keys(report.sections).map((name, i) => {
+       report.sections.map((name, i) => {
             // Generate states for every section
             this.setState({ ["collapse"+i]: true })
             // Text Editor
-            if(report.sections[name] !== "" && report.sections[name] !== undefined){
+            if(report.sections[i].text !== "" && report.sections[i].text !== undefined){
                 // Save current section to text editor
-                this.setState({ ["value"+i]: RichTextEditor.createValueFromString(report.sections[name], 'html') })
+                this.setState({ ["value"+i]: RichTextEditor.createValueFromString(report.sections[i].text, 'html') })
                 return true;
             } else {
                 // Create empty value if the section has no text
@@ -117,7 +126,7 @@ class ReportRevision extends React.Component{
             }
             return true;
         })
-        let count = Object.keys(report.sections).length;
+        let count = report.sections.length;
         return (reviewed / count) * 100;
     }
 
@@ -169,13 +178,13 @@ class ReportRevision extends React.Component{
                     }
                         
                     </MDBProgress>
-                    {Object.keys(report.sections).map((name, i) => {
+                    {report.sections.map((name, i) => {
                         return(
                             <MDBCard key={i} className="mt-3">
                                 <MDBCollapseHeader>
                                     <MDBRow className="justify-content-center">
                                         <MDBCol md="6" className="align-self-center section-title">
-                                            {name}
+                                            {name.title}
                                         </MDBCol>
                                         <MDBCol md="6" className="text-right">
                                             <MDBInput label="Überprüft" filled type="checkbox" id={"checkbox"+i} onClick={this.toggleCollapse("collapse"+i)} />
