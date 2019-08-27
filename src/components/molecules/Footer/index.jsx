901a91befb0717746e2dfbfad5698f2b2f1afa94
Christian Aichner <contact@aichner-christian.com>
2019-08-28 01:48:43 +0200

    Improve footer
    The footer now displays different content for
    coaches and customers. It has also been improved
    to display more meaningful content. The made by
    notice has been removed because of a request of
    the client.

--- src/components/molecules/Footer/index.jsx
+++ src/components/molecules/Footer/index.jsx
@@ -14,22 +14,34 @@ import {
 
 class Footer extends React.Component{
     render(){
+        // Get global state with login information
+        const { globalState } = this.props;
+
         return(
             <MDBFooter color="unique-darkblue" className="mt-4">
                 <div className="py-4">
                     <MDBContainer>
-                        <MDBRow className="text-center">
-                            <MDBCol md="4">
-                                <a href="#">Referral Programm</a>
-                            </MDBCol>
-                            <MDBCol md="4">
-                                <a href="#"><MDBIcon fab icon="facebook-f" className="pr-2" /></a>
-                                <a href="#"><MDBIcon fab icon="instagram" className="pl-2" /></a>
-                            </MDBCol>
-                            <MDBCol md="4">
-                                <a href="#">Fragen? Wir sind für Sie da!</a>
-                            </MDBCol>
-                        </MDBRow>
+                        {!globalState.coach ? (
+                                <MDBRow className="text-center flex-center">
+                                    <MDBCol md="4">
+                                        <a href="#!">Referral Programm</a>
+                                    </MDBCol>
+                                    <MDBCol md="4">
+                                        <a href="#!"><MDBIcon fab icon="facebook-f" className="pr-2" /></a>
+                                        <a href="#!"><MDBIcon fab icon="instagram" className="pl-2" /></a>
+                                    </MDBCol>
+                                    <MDBCol md="4">
+                                        <a href="#!">Fragen? Wir sind für Sie da!</a>
+                                    </MDBCol>
+                                </MDBRow>
+                            ) : (
+                                <MDBRow className="text-center flex-center">
+                                    <MDBCol md="4">
+                                        <a href="https://kisy.aichner-christian.com/?goto=Charm&token=asdf" target="_blank" rel="noopener noreferrer"><MDBIcon icon="medkit" className="pr-2" />Technischer Support</a>
+                                    </MDBCol>
+                                </MDBRow>
+                            )
+                        }
                     </MDBContainer>
                 </div>
                 <div className="footer-copyright text-center py-3">
