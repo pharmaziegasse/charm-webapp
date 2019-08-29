9fd231ad626adeee5fe7282c31de20949d8be70f
Christian Aichner <contact@aichner-christian.com>
2019-08-29 23:51:10 +0200

    Improve Login
    The customer can now choose the login method.
    E-Mail or Phone is now possible. The selection is
    saved to localStorage, so the user experience is
    enhanced.

--- src/components/pages/Login/index.jsx
+++ src/components/pages/Login/index.jsx
@@ -14,6 +14,7 @@ import {
     MDBCardBody,
     MDBBtn,
     MDBAlert,
+    MDBIcon,
 } from "mdbreact";
 
 //> Additional libraries
@@ -25,6 +26,9 @@ import 'react-phone-input-2/dist/style.css';
 // Apollo
 import { graphql, Query } from "react-apollo";
 import { gql } from "apollo-boost";
+
+//> CSS
+import './login.scss';
 
 //> Queries and Mutations
 const LOGIN_USER = gql`
@@ -40,7 +44,18 @@ class Login extends React.Component {
         super(props);
         this.state = {
             email: "",
+            phone: "",
             password: "",
+            method: 'email',
+        }
+    }
+
+    componentWillMount = () => {
+        let method = localStorage.getItem('method');
+        if(method){
+            this.setState({
+                method: method
+            });
         }
     }
 
@@ -76,6 +91,45 @@ class Login extends React.Component {
         });
     };
 
+    handlePhoneChange = (value) => {
+        if(this.state.method === 'phone'){
+            if(this.state.email === ""){
+                this.setState({
+                    phone: value
+                })
+            } else {
+                this.setState({
+                    phone: value,
+                    email: ""
+                });
+            }
+            
+        }
+    }
+
+    handleEmailChange = (e) => {
+        if(this.state.method === 'email'){
+            if(this.state.phone === ""){
+                this.setState({
+                    email: e.target.value
+                })
+            } else {
+                this.setState({
+                    email: e.target.value,
+                    phone: ""
+                });
+            }
+        }
+    }
+
+    switchMethod = (method) => {
+        // Update state and set method
+        this.setState({
+            method: method
+        }, () => localStorage.setItem('method',method));
+    }
+
+
     render() {
 
         // Get global state with login information
@@ -94,33 +148,70 @@ class Login extends React.Component {
         return (
         <div>
             <MDBEdgeHeader color="secondary-color lighten-3" />
-            <MDBFreeBird>
+            <MDBFreeBird id="login">
                 <MDBRow>
                     <MDBCol
                     md="10"
                     className="mx-auto float-none white z-depth-1 py-2 px-2"
                     >
                         <MDBCardBody>
-                            <MDBRow className="justify-content-center">
+                            <MDBRow className="flex-center">
                                 <MDBCol md="6">
                                     <form onSubmit={this.handleSubmit} className="needs-validation" noValidate>
-                                        <p className="h4 text-center mb-4">Sign in</p>
-                                        <label htmlFor="defaultFormLoginEmailEx" className="grey-text">
-                                        Your email
-                                        </label>
-                                        <input
-                                        type="email"
-                                        id="defaultFormLoginEmailEx"
-                                        className="form-control"
-                                        onChange={this.handleChange}
-                                        required
-                                        />
-                                        <div className="invalid-feedback">
-                                            Please enter an E-Mail
-                                        </div>
+                                        <p className="h4 text-center mb-4">Einloggen</p>
+                                        {this.state.method === 'email' ? (
+                                            <>
+                                                <label htmlFor="defaultFormLoginEmailEx" className="grey-text">
+                                                Deine E-Mail Adresse
+                                                </label>
+                                                <input
+                                                type="email"
+                                                id="defaultFormLoginEmailEx"
+                                                className="form-control"
+                                                value={this.state.email}
+                                                onChange={this.handleEmailChange}
+                                                required
+                                                />
+                                                <div className="invalid-feedback">
+                                                    Bitte gib deine E-Mail Adresse an.
+                                                </div>
+                                                <div className="text-right">
+                                                    <a
+                                                    role="button"
+                                                    className="blue-text"
+                                                    onClick={() => this.switchMethod('phone')}
+                                                    >
+                                                    Über Telefonnummer einloggen
+                                                    </a>
+                                                </div>
+                                                
+                                            </>
+                                        ) : (
+                                            <>
+                                                <label htmlFor="pho">Telefon Nummer</label>
+                                                <ReactPhoneInput
+                                                defaultCountry={'at'}
+                                                preferredCountries={['at','de','ch']}
+                                                value={this.state.phone}
+                                                onChange={this.handlePhoneChange}
+                                                inputExtraProps={{
+                                                    className: 'w-100'
+                                                }}
+                                                />
+                                                <div className="text-right">
+                                                    <a
+                                                    role="button"
+                                                    className="blue-text"
+                                                    onClick={() => this.switchMethod('email')}
+                                                    >
+                                                    Über E-Mail einloggen
+                                                    </a>
+                                                </div>
+                                            </>
+                                        ) }
                                         <br />
                                         <label htmlFor="defaultFormLoginPasswordEx" className="grey-text">
-                                        Your password
+                                        Dein Passwort
                                         </label>
                                         <input
                                         type="password"
@@ -130,16 +221,16 @@ class Login extends React.Component {
                                         required
                                         />
                                         <div className="invalid-feedback">
-                                            Please enter a password
+                                            Bitte gib ein Passwort an.
                                         </div>
                                         <div className="text-center mt-4">
                                             <MDBBtn color="success" type="submit">
-                                            <i className="fas fa-key pr-2"></i>Login
+                                                <MDBIcon icon="angle-right" className="pr-2" />Einloggen
                                             </MDBBtn>
                                         </div>
                                         <p className="text-muted text-center mt-3">
-                                        Not a member yet? No problem, just <Link to="/join">
-                                        <strong>join us</strong>
+                                        Noch kein Mitglied? Jetzt einfach <Link to="/join">
+                                        <strong>Mitglied werden</strong>
                                         </Link>!
                                         </p>
                                     </form>
